bd8bbfa4a13842da3b4c000fe1ef8e3a9e94b1bc
rolf0504 <fangrolf@gmail.com>
2021-01-06 13:58:00 +0800

    isjc

--- createDB.py
+++ createDB.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlite3
 
-db = sqlite3.connect('rest.db')
-dfs = pd.read_excel('rest.xlsx', sheet_name=None)
+db = sqlite3.connect('./db/rest.db')
+dfs = pd.read_excel('./db/rest.xlsx', sheet_name=None)
 for table, df in dfs.items():
     df.to_sql(table, db)
